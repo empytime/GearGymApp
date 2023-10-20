3c9a9cdad5aa9f4e61ed637296cbe0827bb0cc91
empytime <128811342+empytime@users.noreply.github.com>
2023-10-19 23:12:18 -0300

    Add api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBClBOuuEQXEgzNzPsuXTI8l_ZoC-RBpjU",
+    authDomain: "bdgeargymapp.firebaseapp.com",
+    projectId: "bdgeargymapp",
+    storageBucket: "bdgeargymapp.appspot.com",
+    messagingSenderId: "246255648011",
+    appId: "1:246255648011:web:1156a8a1264773d4a2360e"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBClBOuuEQXEgzNzPsuXTI8l_ZoC-RBpjU",
+    authDomain: "bdgeargymapp.firebaseapp.com",
+    projectId: "bdgeargymapp",
+    storageBucket: "bdgeargymapp.appspot.com",
+    messagingSenderId: "246255648011",
+    appId: "1:246255648011:web:1156a8a1264773d4a2360e"
+  }
 };
 
 /*
